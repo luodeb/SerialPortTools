bf259602d0635fc9147d69e0a231b75808a8c0a5
翔大村夫 <864376923@qq.com>
2022-05-26 00:58:46 +0800

    桌面端框架1.0

---
+++ lib/main.dart
@@ -0,0 +1,326 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_acrylic/flutter_acrylic.dart' as flutter_acrylic;
+import 'package:provider/provider.dart';
+import 'package:system_theme/system_theme.dart';
+import 'package:url_strategy/url_strategy.dart';
+import 'package:window_manager/window_manager.dart';
+
+import 'package:serial_ports_debugging_tools_client/pages/COM_1.dart';
+import 'package:serial_ports_debugging_tools_client/pages/COM_2.dart';
+import 'package:serial_ports_debugging_tools_client/pages/COM_3.dart';
+import 'package:serial_ports_debugging_tools_client/pages/COM_4.dart';
+import 'package:serial_ports_debugging_tools_client/pages/COM_5.dart';
+import 'package:serial_ports_debugging_tools_client/pages/COM_6.dart';
+import 'package:serial_ports_debugging_tools_client/pages/COM_7.dart';
+import 'package:serial_ports_debugging_tools_client/pages/COM_8.dart';
+import 'package:serial_ports_debugging_tools_client/pages/COM_9.dart';
+import 'package:serial_ports_debugging_tools_client/pages/settings.dart';
+import 'theme.dart';
+
+const String appTitle = '  串口调试助手  ';
+
+/// Checks if the current environment is a desktop environment.
+bool get isDesktop {
+  if (kIsWeb) return false;
+  return [
+    TargetPlatform.windows,
+    TargetPlatform.linux,
+    TargetPlatform.macOS,
+  ].contains(defaultTargetPlatform);
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // if it's on the web, windows or android, load the accent color
+  if (kIsWeb ||
+      [TargetPlatform.windows, TargetPlatform.android]
+          .contains(defaultTargetPlatform)) {
+    SystemTheme.accentColor.load();
+  }
+
+  setPathUrlStrategy();
+
+  if (isDesktop) {
+    await flutter_acrylic.Window.initialize();
+    await WindowManager.instance.ensureInitialized();
+    windowManager.waitUntilReadyToShow().then((_) async {
+      await windowManager.setTitleBarStyle(
+        TitleBarStyle.hidden,
+        windowButtonVisibility: false,
+      );
+      await windowManager.setSize(const Size(1000, 750));
+      await windowManager.setMinimumSize(const Size(500, 375));
+      await windowManager.center();
+      await windowManager.show();
+      await windowManager.setPreventClose(true);
+      await windowManager.setSkipTaskbar(false);
+    });
+  }
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => AppTheme(),
+      builder: (context, _) {
+        final appTheme = context.watch<AppTheme>();
+        return FluentApp(
+          title: appTitle,
+          themeMode: appTheme.mode,
+          debugShowCheckedModeBanner: false,
+          home: const MyHomePage(),
+          color: appTheme.color,
+          darkTheme: ThemeData(
+            brightness: Brightness.dark,
+            accentColor: appTheme.color,
+            visualDensity: VisualDensity.standard,
+            focusTheme: FocusThemeData(
+              glowFactor: is10footScreen() ? 2.0 : 0.0,
+            ),
+          ),
+          theme: ThemeData(
+            accentColor: appTheme.color,
+            visualDensity: VisualDensity.standard,
+            focusTheme: FocusThemeData(
+              glowFactor: is10footScreen() ? 2.0 : 0.0,
+            ),
+          ),
+          builder: (context, child) {
+            return Directionality(
+              textDirection: appTheme.textDirection,
+              child: NavigationPaneTheme(
+                data: NavigationPaneThemeData(
+                  backgroundColor: appTheme.windowEffect !=
+                      flutter_acrylic.WindowEffect.disabled
+                      ? Colors.transparent
+                      : null,
+                ),
+                child: child!,
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with WindowListener {
+  bool value = false;
+
+  int index = 0;
+
+  final settingsController = ScrollController();
+  final viewKey = GlobalKey();
+
+  @override
+  void initState() {
+    windowManager.addListener(this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    windowManager.removeListener(this);
+    settingsController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final appTheme = context.watch<AppTheme>();
+    return NavigationView(
+      key: viewKey,
+      appBar: NavigationAppBar(
+        automaticallyImplyLeading: false,
+        title: () {
+          if (kIsWeb) return const Text(appTitle);
+          return const DragToMoveArea(
+            child: Align(
+              alignment: AlignmentDirectional.centerStart,
+              child: Text(appTitle,textScaleFactor: 1.5,),
+            ),
+          );
+        }(),
+        actions: kIsWeb
+            ? null
+            : Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [Spacer(), WindowButtons()],
+        ),
+      ),
+      pane: NavigationPane(
+        selected: index,
+        onChanged: (i) => setState(() => index = i),
+        size: const NavigationPaneSize(
+          //openMinWidth: 150,
+          //openMaxWidth: 220,
+          openWidth: 200,
+        ),
+        header: Container(
+          height: kOneLineTileHeight,
+          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+          alignment: Alignment.centerLeft,
+          child: const Text(
+            "串口列表",
+            style: TextStyle(fontWeight: FontWeight.bold),
+            textScaleFactor: 1.2,
+          ),
+          /*
+          FlutterLogo(
+            style: appTheme.displayMode == PaneDisplayMode.top
+                ? FlutterLogoStyle.markOnly
+                : FlutterLogoStyle.horizontal,
+            size: appTheme.displayMode == PaneDisplayMode.top ? 24 : 100.0,
+          ),
+          */
+        ),
+        displayMode: appTheme.displayMode,
+        indicator: () {
+          switch (appTheme.indicator) {
+            case NavigationIndicators.end:
+              return const EndNavigationIndicator();
+            case NavigationIndicators.sticky:
+            default:
+              return const StickyNavigationIndicator();
+          }
+        }(),
+        items: [
+          // It doesn't look good when resizing from compact to open
+          // PaneItemHeader(header: Text('User Interaction')),
+          PaneItem(
+            icon: const Icon(FluentIcons.devices3),
+            title: const Text('COM1'),
+          ),
+          PaneItem(
+            icon: const Icon(FluentIcons.devices3),
+            title: const Text('COM2'),
+          ),
+          PaneItem(
+            icon: const Icon(FluentIcons.devices3),
+            title: const Text('COM3'),
+          ),
+          PaneItem(
+            icon: const Icon(FluentIcons.devices3),
+            title: const Text('COM4'),
+          ),
+          PaneItem(
+            icon: const Icon(FluentIcons.devices3),
+            title: const Text('COM5'),
+          ),
+          PaneItem(
+            icon: const Icon(FluentIcons.devices3),
+            title: const Text('COM6'),
+          ),
+          PaneItem(
+            icon: const Icon(FluentIcons.devices3),
+            title: const Text('COM7'),
+          ),
+          PaneItem(
+            icon: const Icon(FluentIcons.devices3),
+            title: const Text('COM8'),
+          ),
+          PaneItem(
+            icon: const Icon(FluentIcons.devices3),
+            /*
+            icon: Icon(
+              appTheme.displayMode == PaneDisplayMode.top
+                  ? FluentIcons.more
+                  : FluentIcons.more_vertical,
+            ),
+            */
+            title: const Text('COM9'),
+            /*
+            infoBadge: const InfoBadge(
+              source: Text('9'),
+            ),
+            */
+          ),
+        ],
+        footerItems: [
+          PaneItemSeparator(),
+          PaneItem(
+            icon: const Icon(FluentIcons.settings),
+            title: const Text('Settings'),
+          ),
+        ],
+      ),
+      content: NavigationBody(index: index, children: [
+        const COM1(),
+        const COM2(),
+        const COM3(),
+        const COM4(),
+        const COM5(),
+        const COM6(),
+        const COM7(),
+        const COM8(),
+        const COM9(),
+        Settings(controller: settingsController),
+      ]),
+    );
+  }
+
+  @override
+  void onWindowClose() async {
+    bool _isPreventClose = await windowManager.isPreventClose();
+    if (_isPreventClose) {
+      showDialog(
+        context: context,
+        builder: (_) {
+          return ContentDialog(
+            title: const Text('退出程序'),
+            content: const Text('您确定要退出吗？'),
+            actions: [
+              FilledButton(
+                child: const Text('是'),
+                onPressed: () {
+                  Navigator.pop(context);
+                  windowManager.destroy();
+                },
+              ),
+              Button(
+                child: const Text('否'),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+}
+
+class WindowButtons extends StatelessWidget {
+  const WindowButtons({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = FluentTheme.of(context);
+
+    return SizedBox(
+      width: 138,
+      height: 50,
+      child: WindowCaption(
+        brightness: theme.brightness,
+        backgroundColor: Colors.transparent,
+      ),
+    );
+  }
+}
+
---
+++ lib/pages/COM_7.dart
@@ -0,0 +1,206 @@
+import 'package:fluent_ui/fluent_ui.dart';
+
+const Widget spacerH = SizedBox(height: 5.0);
+const Widget spacerW = SizedBox(width: 5.0);
+const Widget spacerHL = SizedBox(height: 15.0);
+const Widget spacerWL = SizedBox(width: 30.0);
+
+class COM7 extends StatefulWidget{
+  const COM7({Key? key}) : super(key: key);
+
+  @override
+  State createState() {
+    return _COM7State();
+  }
+}
+
+class _COM7State extends State<COM7> {
+  bool disabled = false;
+
+  bool valueOfHEXBtn = false;
+  bool valueOfTextBtn = false;
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ScaffoldPage(
+      header: PageHeader(
+        title: const Text("COM7"),
+        commandBar: ToggleSwitch(
+          checked: disabled,
+          onChanged: (v) => setState(() => disabled = v),
+          content: disabled ? const Text("On") : const Text("Off"),
+        ),
+      ),
+      content: Column(
+        children: [
+          spacerHL,
+          ConstrainedBox(
+            constraints: const BoxConstraints(maxHeight: 100,minWidth: double.infinity),
+            child: Card(
+              child: Wrap(
+                //alignment: WrapAlignment.start,//TODO 根据窗口大小选择排序方式
+                children: [
+                  spacerW,
+                  const Text("波特率"),
+                  spacerW,
+                  DropDownButton(
+                    disabled: !disabled,
+                    title: Text("9600"),//TODO 返回选取值
+                    items: [
+                      MenuFlyoutItem(
+                        text: const Text('300'),
+                        onPressed: (){},
+                      ),
+                      MenuFlyoutItem(
+                        text: const Text('1200'),
+                        onPressed: (){},
+                      ),
+                      MenuFlyoutItem(
+                        text: const Text('2400'),
+                        onPressed: (){},
+                      ),
+                      MenuFlyoutItem(
+                        text: const Text('9600'),
+                        onPressed: (){},
+                      ),
+                      MenuFlyoutItem(
+                        text: const Text('19200'),
+                        onPressed: (){},
+                      ),
+                      MenuFlyoutItem(
+                        text: const Text('38400'),
+                        onPressed: (){},
+                      ),
+                      MenuFlyoutItem(
+                        text: const Text('115200'),
+                        onPressed: (){},
+                      ),
+                      MenuFlyoutItem(
+                        text: const Text('自定义'),
+                        onPressed: (){},
+                      ),
+                    ],
+                  ),
+                  spacerWL,
+                  const Text("数字位"),
+                  spacerW,
+                  DropDownButton(
+                    disabled: !disabled,
+                    title: Text("8"),//TODO 返回选取值
+                    items: [
+                      MenuFlyoutItem(
+                        text: const Text('2'),
+                        onPressed: (){},
+                      ),
+                      MenuFlyoutItem(
+                        text: const Text('8'),
+                        onPressed: (){},
+                      ),
+                      MenuFlyoutItem(
+                        text: const Text('16'),
+                        onPressed: (){},
+                      ),
+                      MenuFlyoutItem(
+                        text: const Text('自定义'),
+                        onPressed: (){},
+                      ),
+                    ],
+                  ),
+                  spacerWL,
+                  const Text("校验位"),
+                  spacerW,
+                  DropDownButton(
+                    disabled: !disabled,
+                    title: Text("8"),//TODO 返回选取值
+                    items: [
+                      MenuFlyoutItem(
+                        text: const Text('2'),
+                        onPressed: (){},
+                      ),
+                      MenuFlyoutItem(
+                        text: const Text('8'),
+                        onPressed: (){},
+                      ),
+                      MenuFlyoutItem(
+                        text: const Text('16'),
+                        onPressed: (){},
+                      ),
+                      MenuFlyoutItem(
+                        text: const Text('自定义'),
+                        onPressed: (){},
+                      ),
+                    ],
+                  ),
+                  spacerWL,
+                  const Text("奇偶位"),
+                  spacerW,
+                  DropDownButton(
+                    disabled: !disabled,
+                    title: Text("8"),//TODO 返回选取值
+                    items: [
+                      MenuFlyoutItem(
+                        text: const Text('2'),
+                        onPressed: (){},
+                      ),
+                      MenuFlyoutItem(
+                        text: const Text('8'),
+                        onPressed: (){},
+                      ),
+                      MenuFlyoutItem(
+                        text: const Text('16'),
+                        onPressed: (){},
+                      ),
+                      MenuFlyoutItem(
+                        text: const Text('自定义'),
+                        onPressed: (){},
+                      ),
+                    ],
+                  ),
+                  spacerWL,
+                  RadioButton(
+                    checked: valueOfHEXBtn,
+                    onChanged: !disabled ? null : (v) => setState(() => valueOfHEXBtn = v),
+                    content: Text("HEX"),
+                  ),
+                  spacerWL,
+                  RadioButton(
+                    checked: valueOfTextBtn,
+                    onChanged: !disabled ? null : (v) => setState(() => valueOfTextBtn = v),
+                    content: Text("Text"),
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+          spacerHL,
+
+          Expanded(
+            child: TextFormBox(
+              maxLines: null,
+              suffixMode: OverlayVisibilityMode.always,
+              minHeight: double.infinity,
+              expands: true,
+              placeholder: '数据展示区',
+            ),
+          ),
+
+          ConstrainedBox(
+            constraints: const BoxConstraints(maxHeight: 100),
+            child: TextFormBox(
+              maxLines: null,
+              suffixMode: OverlayVisibilityMode.always,
+              minHeight: 100,
+              expands: true,
+              placeholder: '命令行交互区',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
