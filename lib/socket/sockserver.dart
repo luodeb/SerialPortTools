11c4ed639db135f62a8194303f4c3c50fdfa67cd
chenzhixin <1041446659@qq.com>
2022-05-27 18:54:37 +0800

    socket_server_cmd

--- lib/socket/sockserver.dart
+++ lib/socket/sockserver.dart
@@ -8,9 +8,9 @@ import 'dart:convert';
  * 1、端口占用
  * 2、传输超时重置、客户端不正常造成数据混乱重置等
  */
-void startServer(){
+void startServer(String ip,int port){
   ServerSocket
-  .bind('127.0.0.1', 4041) //绑定端口4041，根据需要自行修改，建议用动态，防止端口占用
+  .bind(ip, port) //绑定端口4041，根据需要自行修改，建议用动态，防止端口占用
   .then((serverSocket) {
       serverSocket.listen((socket) {//第一个监听监听套接字是否连接，第二个监听监听数据？
           var tmpData="";
@@ -21,16 +21,18 @@ void startServer(){
           socket.cast<List<int>>().transform(utf8.decoder).listen((s) {
            
             tmpData = doParseResultJson(socket, tmpData, s);
+            tmpData = '';
+            s = '';
           });
         }
       );
     }
   );
  
-  print(DateTime.now().toString() + " Socket服务启动，正在监听端口 4041...");
+  print(DateTime.now().toString() + " Socket服务启动，正在监听端口 "+port.toString()+"....");
 }
  
-/**
+/*ge
  * 按JSON格式进行解析收到的结果，无论是否粘包，都是可进行解析
  * sData：为已经收到的临时数据
  * s：为当前收到的数据
@@ -47,10 +49,11 @@ String doParseResultJson(Socket socket, String sData, String s){
   // 找这个串里有没有相应的JSON符号
   // 没有的话，将数据返回等下一个包
   var bHasJSON = tmpData.contains("{") && tmpData.contains("}"); 
+  // var bHasJSON = tmpData.contains("COM") ; 
   if (!bHasJSON) {
     return tmpData;
   }
- 
+  
   //找有类似JSON串，看"{"是否在"}"的前面，
   //在前面，则解析，解析失败，则继续找下一个"}"
   //解析成功，则进行业务处理
@@ -63,60 +66,57 @@ String doParseResultJson(Socket socket, String sData, String s){
     log(socket, '{}=>' + idxStart.toString() + "--" + idxEnd.toString());
     if (idxStart >= idxEnd) {
       continue;// 找下一个 "}"
-    }
- 
-    var sJSON = tmpData.substring(idxStart, idxEnd); 
+    }  
+    var sJSON = tmpData.substring(idxStart, idxEnd);//提取 {指令} 
     log(socket, "解析 JSON ...." + sJSON);
     try{
       var jsondata = jsonDecode(sJSON); //解析成功，则说明结束，否则抛出异常，继续接收
       log(socket, "解析 JSON OK :" + jsondata.toString());
  
-      ///此处加入你要处理的业务方法，一般调用另外一个方法进行下一步处理
+      ///指令解析
       doCommand(socket, jsondata);
       
- 
+
       tmpData = tmpData.substring(idxEnd); //剩余未解析部分
       idxEnd = 0; //复位
-      
+     
       if (tmpData.contains("{") && tmpData.contains("}")) {
         tmpData = doParseResultJson(socket, tmpData, "");
         break;
       }
+      return tmpData;
     } catch(err) {
       log(socket, "解析 JSON 出错:" + err.toString() + ' waiting for next "}"....'); //抛出异常，继续接收，等下一个}
+     
     }
   }
   return tmpData;
 }
  
 /**
- * 举例，支持的几个命令 current time, XX, 天气
- * current time：问当前时间，就看一下是北京的还是伦敦的
- * xx：返回YY
- * 天气：返回固定多云转阴天，有大雨！
+ 与上层client协议
  */
 void doCommand(Socket clientsocket, jsonData) {
-  var command = jsonData['cmd'].toString().toUpperCase();
+
+  var command = jsonData['func'].toString().toUpperCase();
   switch (command) {
-    case 'CURRENT TIME':
-      var region = jsonData['params']['region'];
-      if (region == '北京') {
-        clientsocket.write (region + '时间：' + DateTime.now().toString());
-      } else if (region == '伦敦') {
-        clientsocket.write(region + '时间：' + DateTime.now().add(Duration(hours:-8)).toString());
-      } else  {
-        clientsocket.write (region + '时间：' + DateTime.now().toString());
-      }
-      break;
-    case 'XX':
-      clientsocket.write(command + " result YY");
-      break;
-    case '天气':
-      clientsocket.write(command + ":多云转阴天，有大雨！");
-      break;
-    default:
-      clientsocket.write("不认识：command " + command);
+    case 'scan':
+       var L = {
+                "func":"scan",
+                "comNum":2,
+                "com":{
+                    "comName":"COM10",
+                    "comMess":"虚拟串口COM10->COM11",
+                      },
+                "com":{
+                    "comName":"COM11",
+                    "comMess":"虚拟串口COM10->COM11",
+                },
+              };
+       clientsocket.write (L);
+
   }
+   
 }
  
 void log(Socket socket, logdata) {
@@ -127,5 +127,5 @@ void log(Socket socket, logdata) {
  * 主方法入口
  */
 void main(){
-  startServer();
+  startServer('127.0.0.1',4041);
 }
