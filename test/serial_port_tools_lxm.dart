e7e6e903a4233a26e4fe7211248882f0dde37e30
luodeb <luodeb@outlook.com>
2022-05-31 00:17:05 +0800

    test

--- test/serial_port_tools_lxm.dart
+++
@@ -1,8 +0,0 @@
-import 'package:serial_port_tools/serial_port_tools.dart';
-import 'package:test/test.dart';
-//我是测试push
-void main() {
-  test('calculate', () {
-     expect(calculate(), 42);
-  });
-}
