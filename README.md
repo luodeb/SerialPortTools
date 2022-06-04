810c47a1c09d2e1777be6557d76641306ba66973
luodeb <luodeb@outlook.com>
2022-06-04 15:10:18 +0800

    socket server完善一下

--- README.md
+++ README.md
@@ -1,16 +1,184 @@
-# serial_ports_debugging_tools_client
+# 串口调试助手
 
-A new Flutter project.
+## 说明
 
-## Getting Started
+厦门大学高级编程技术课程大作业 使用Dart实现的跨平台串口调试助手应用。
 
-This project is a starting point for a Flutter application.
+## 运行前提
 
-A few resources to get you started if this is your first Flutter project:
+ - 需要按照网上配置好dart环境
+ - 安装好[git](https://git-scm.com/)
+ - 安装好MX虚拟串口软件，使用里面的注册机激活
+ - 使用```git clone https://gitee.com/luodeb/serial-port-tools.git```克隆项目
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+## 运行
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+查看bin文件夹下的serial_port_tools.dart文件，这里我使用的COM10，所以我们创建串口对COM10-COM11
+
+![README-2022-05-25-17-25-44](https://imgfiles-debin.oss-cn-hangzhou.aliyuncs.com/md_imgfiles/README-2022-05-25-17-25-44.png)
+
+打开串口调试助手，打开COM11
+![README-2022-05-25-17-27-01](https://imgfiles-debin.oss-cn-hangzhou.aliyuncs.com/md_imgfiles/README-2022-05-25-17-27-01.png)
+
+然后在项目的根目录运行```dart run```就可以了。
+
+## 需求（后端部分）
+
+### serialPort类编写
+
+名称：class serialPort 
+功能：
+ - .comName 端口号
+ - .comBind 波特率
+ - ...其他参数待定
+ - .send(data) 向串口发送uint8List数据
+ - .connect(comName) 连接串口
+ - .disconnect() 断开当前串口
+ - .check() return portList 返回串口列表
+ - .recive() return data 接收uint8List数据
+
+### socket类编写
+
+名称：class serialSocket
+功能：
+ - .address IP地址，默认为localhost
+ - .localPort 本地端口号
+ - .remotePort 远程端口号
+ - .initSocket() 初始化socket
+ - .send(jsonData) 发送json数据
+ - .recive() return jsonData接收数据
+
+### 通讯协议
+
+名称：class serialProtocol()
+功能：
+ - .pack(data) return jsonData将uint8List数据打包成socket发送的json数据
+ - .unpack(jsonData) 根据接收到的数据解析socket需要发送的数据
+设计：
+需要设计相应的通讯协议。例如
+``` json
+// 扫描串口
+{
+    "func":"scan",
+    "comNum":2,
+    "com":{
+        "comName":"COM10",
+        "comMess":"ELTIMA Virtual Serial Port",
+    },
+    "com":{
+        "comName":"COM11",
+        "comMess":"ELTIMA Virtual Serial Port",
+    },
+}
+
+// 连接串口COM10
+{
+    "func":"connect",
+    "status":"true",
+    "com":{
+        "comName":"COM10",
+        "comBind":115200,
+    },
+}
+
+// 向串口10发送数据1415926
+{
+    "func":"send",
+    "type":{
+        "comName":"COM10",
+        "comBind":115200,
+    },
+    "data":"1415926",
+}
+```
+
+## 需求（前端部分）
+
+test
+
+## 前端通讯协议解析
+
+### 读取串口列表
+前端发送:
+``` json
+{
+    "func":"scan"
+}
+```
+后端返回：
+``` json 
+{
+    "func":"scan",
+    "comNum":2,
+    "com":[{
+        "comName":"COM10",
+        "comMess":"虚拟串口COM10->COM11",
+    },{
+        "comName":"COM11",
+        "comMess":"虚拟串口COM10->COM11",
+    },]
+}
+
+### 打开串口COM10 
+前端发送:
+``` json
+{
+    "func":"connect",
+    "com":{
+        "comName":"COM10",
+        "comBind":115200,
+    }
+}
+```
+后端返回：
+ - 成功：
+``` json 
+{
+    "func":"connect",
+    "type":"true",
+    "com":{
+        "comName":"COM10",
+        "comBind":115200,
+    }
+}
+```
+ - 失败：
+``` json 
+{
+    "func":"connect",
+    "type":"false",
+    "com":{
+        "comName":"COM10",
+        "comBind":115200,
+    }
+}
+```
+同理，关闭串口COM10，将上面的`connect`改为`disconnect`，关闭成功同样返回true
+
+### 发送数据到串口COM10
+
+前端发送:
+``` json
+{
+    "func":"send",
+    "com":{
+        "comName":"COM10",
+        "comBind":115200,
+    },
+    "data":"666666"
+}
+```
+
+### 从串口接收数据
+
+后端发送：
+``` json
+{
+    "func":"send",
+    "com":{
+        "comName":"COM10",
+        "comBind":115200,
+    },
+    "data":"666666"
+}
+```
--- lib/socket/server_socket.dart
+++ lib/socket/server_socket.dart
@@ -1,9 +1,14 @@
+// ignore_for_file: avoid_print
+
 import 'dart:io';
 import 'dart:convert';
+import 'package:flutter_libserialport/flutter_libserialport.dart';
+import 'dart:typed_data';
 
 late ServerSocket mysocket;
-
+late SerialPort myport;
 bool isContinueSendSteam = false; //是否可以发送Steam
+
 
 Future<void> socketBind(String ip, int port) async {
   print('Socket绑定IP $ip : $port');
@@ -13,9 +18,159 @@ Future<void> socketBind(String ip, int port) async {
     var tmpData = "";
     print('连接socket');
     socket.cast<List<int>>().transform(utf8.decoder).listen((s) {
-      // tmpData = doParseResultJson(socket, tmpData, s);
+      tmpData = doParseResultJson(socket, tmpData, s);
       print(s);
       socket.write("test");
     });
   });
 }
+
+/// 按JSON格式进行解析收到的结果，无论是否粘包，都是可进行解析
+/// sData：为已经收到的临时数据
+/// s：为当前收到的数据
+/// 返回结果为未处理的所有数据。
+//接收串口数据超过一个字节会出错
+String doParseResultJson(Socket socket, String sData, String s) {
+  var tmpData = sData + s;
+
+  //log(socket, "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+  log(socket, s);
+  log(socket, "-----------------------------------------");
+  log(socket, tmpData);
+  log(socket, ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+  // 找这个串里有没有相应的JSON符号
+  // 没有的话，将数据返回等下一个包
+  var bHasJSON = tmpData.contains("{") && tmpData.contains("}");
+  if (!bHasJSON) {
+    return tmpData;
+  }
+
+  //找有类似JSON串，看"{"是否在"}"的前面，
+  //在前面，则解析，解析失败，则继续找下一个"}"
+  //解析成功，则进行业务处理
+  //处理完成，则对剩余部分递归解析，直到全部解析完成（此项一般用不到，仅适用于一次发两个以上的JSON串才需要，
+  //每次只传一个JSON串的情况下，是不需要的）
+  int idxStart = tmpData.indexOf("{");
+  int tempIndex = idxStart + 1;
+  int idxEnd = 0;
+  while (tmpData.contains("}", idxEnd)) {
+    idxEnd = tmpData.indexOf("}", idxEnd) + 1;
+
+    if (idxStart >= idxEnd) {
+      continue; // 找下一个 "}"
+    }
+
+    var sJSON = tmpData.substring(idxStart, idxEnd);
+    if (sJSON.contains("{", tempIndex)) //子字符串中仍包含{，则继续检索，解决连续两个{}报错的情况
+    {
+      tempIndex = sJSON.indexOf("{", tempIndex) + 1;
+      continue;
+    }
+    // ignore: prefer_interpolation_to_compose_strings
+    log(socket, '{}=>' + idxStart.toString() + "--" + idxEnd.toString());
+    log(socket, "解析 JSON ....$sJSON");
+    try {
+      var jsondata = jsonDecode(sJSON); //解析成功，则说明结束，否则抛出异常，继续接收
+      log(socket, "解析 JSON OK :$jsondata");
+
+      ///此处加入你要处理的业务方法，一般调用另外一个方法进行下一步处理
+      doCommand(socket, jsondata);
+
+      tmpData = tmpData.substring(idxEnd); //剩余未解析部分
+      idxEnd = 0; //复位
+
+      if (tmpData.contains("{") && tmpData.contains("}")) {
+        tmpData = doParseResultJson(socket, tmpData, "");
+        break;
+      }
+    } catch (err) {
+      log(
+          socket,
+          // ignore: prefer_interpolation_to_compose_strings
+          "解析 JSON 出错:" +
+              err.toString() +
+              ' waiting for next "}"....'); //抛出异常，继续接收，等下一个}
+      tmpData = " "; //解析出错，需清空缓存区数据，若不清除则后续的JASON指令接收会出错，解析的数据一直是粘包的状态
+    }
+  }
+  return tmpData;
+}
+
+
+void log(Socket socket, logdata) {
+  // ignore: prefer_interpolation_to_compose_strings
+  print("${DateTime.now()}[${socket.remoteAddress.address}:${socket.remotePort}]" + logdata);
+}
+
+void doCommand(Socket clientsocket, jsonData) {
+  var command = jsonData['func'].toString().toUpperCase();
+  switch (command) {
+    case "SCAN": //扫描端口
+      {
+        var comJsonTotal = '';
+        var comNum = 0;
+        for (final name in SerialPort.availablePorts) {
+          comNum += 1;
+          final sp = SerialPort(name);
+          var comJson = '"com":{';
+          comJson += '"comName":"$name",';
+          comJson += '"comMess":"${sp.description}"},';
+          // print('\tDescription: ${sp.description}');
+          comJsonTotal += comJson;
+          sp.dispose();
+        }
+        clientsocket
+            .write('{"func":"scan","comNum":$comNum,$comJsonTotal}');
+      }
+      break;
+    case "CONNECT":
+      {
+        var comName = jsonData["com"]["name"].toString();
+        myport = SerialPort(comName);
+        var baudRate = jsonData["com"]["baud"].toString();
+        myport.config.baudRate = int.parse(baudRate);
+        var stopBit = jsonData["com"]["stopBit"].toString();
+        myport.config.stopBits = int.parse(stopBit);
+        var parity = jsonData["com"]["parity"].toString();
+        myport.config.parity = int.parse(parity);
+        if (!myport.openReadWrite()) {
+          print(SerialPort.lastError);
+          clientsocket.write(
+              '{"func":"connect","comName":"$comName","status":"false"}');
+        }
+        // myport.config.baudRate = 115200;
+        clientsocket
+            .write('{"func":"connect","comName":"$comName","status":"true"}');
+        final reader = SerialPortReader(myport);
+
+        reader.stream.listen((data) {
+          try {
+            final String str = String.fromCharCodes(data);
+            clientsocket.write(
+                '{"func":"send","comName":"$comName","data":"$str"}');
+            print('received: $str');
+          } catch (e) {
+            print("error: $data");
+          }
+        });
+      }
+      break;
+    case "SEND":
+      {
+        // var comName = jsonData["comName"].toString();
+        var comData = jsonData["data"].toString();
+        print(comData);
+        myport.write(Uint8List.fromList(comData.codeUnits));
+      }
+      break;
+    case "DISCONNECT":
+      {
+        myport.dispose();
+      }
+      break;
+    default:
+      clientsocket.write("不认识:command $command");
+  }
+}
+
+
